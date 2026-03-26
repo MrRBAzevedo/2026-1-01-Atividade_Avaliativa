b04beb38251e53dcce1f75591a0eaaf56b6810b4
MrRBAzevedo <renan.azevedo@academico.ifrn.edu.br>
2026-03-26 12:30:32 +0000

    Questões 1 a 5

---
+++ questao02.py
@@ -0,0 +1,3 @@
+print('''Meu nome é Fulano.
+Tenho 18 anos.
+Estou no curso de Informática para Internet.''')
---
+++ questao03.py
@@ -0,0 +1,5 @@
+print('''*** Cardápio ***
+1. Pizza
+2. Hambúrguer
+3. Suco
+*** Bom apetite! ***''')
